345776fcfe5f04631596be4bc3a9b2dcc298e405
Edward Spencer <epfspencer@yahoo.com>
2020-10-04 21:14:18 +0100

    [LeanOpenGL] Create a Window

--- Main.cpp
+++ Main.cpp
@@ -1,10 +1,58 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <stdio.h>
+#include <iostream>
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 
 
 int main()
 {
-	printf("hello world");
+	printf("hello window");
+
+	// instatiate a GLFW window
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	// create a window object and set context
+	GLFWwindow* window = glfwCreateWindow(800, 600, "My Beautiful Window", NULL, NULL);
+	if (window == NULL)
+	{
+		std::cout << "Failed to crwate GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+	// initialise GLAD, so we're ready to call OpenGL functions
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialise GLAD" << std::endl;
+		return -1;
+	}
+
+	// set window dimensions for OpenGL
+	glViewport(0, 0, 800, 600);
+
+	// register window resize callback
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+	// start render loop
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	// clean up
+	glfwTerminate();
 	return 0;
+}
+
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, height, width);
 }
