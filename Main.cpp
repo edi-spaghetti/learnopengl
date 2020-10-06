d7b6b2c6a9f6e01935703617d4fbc8a1413e930b
Edward Spencer <epfspencer@yahoo.com>
2020-10-06 21:40:05 +0100

    [LearnOpenGL] Hello Rectangle

--- Main.cpp
+++ Main.cpp
@@ -28,9 +28,15 @@ const char* fragmentShaderSource = "#version 330 core\n"
 
 // vertex data
 const float vertices[] = {
-	-0.5f, -0.5f,  0.0f,
-	 0.5f, -0.5f,  0.0f,
-	 0.0f,  0.5f,  0.0f
+	  0.5f,  0.5f, 0.0f,
+	  0.5f, -0.5f, 0.0f,
+	 -0.5f, -0.5f, 0.0f,
+	 -0.5f,  0.5f, 0.0f
+};
+
+unsigned int indices[] = {
+	0, 1, 3,
+	1, 2, 3
 };
 
 
@@ -46,10 +52,11 @@ int main()
 	int shaderProgram = create_shader_program(vertexShader, fragmentShader);
 
 	//generate array and buffer objects
-	unsigned int VBO, VAO;
+	unsigned int VBO, VAO, EBO;
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
-	
+	glGenBuffers(1, &EBO);
+
 	// bind the vertex array
 	glBindVertexArray(VAO);
 
@@ -61,9 +68,14 @@ int main()
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
 
-	// unbind VBO and VAO
+	// bind and set element buffers
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+	// unbind VBO, VAO and EBO
 	glBindBuffer(GL_ARRAY_BUFFER, 0);	
 	glBindVertexArray(0);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 
 	// start render loop
 	while (!glfwWindowShouldClose(window))
@@ -79,7 +91,14 @@ int main()
 
 		glUseProgram(shaderProgram);
 		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		//glDrawArrays(GL_TRIANGLES, 0, 3);
+		//glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+		glDrawElements(
+			GL_TRIANGLES,
+			sizeof(indices) / sizeof(indices[0]),
+			GL_UNSIGNED_INT,
+			0
+		);
 
 		// check and call events and swap the buffers
 		glfwSwapBuffers(window);
@@ -88,6 +107,7 @@ int main()
 
 	glDeleteVertexArrays(1, &VAO);
 	glDeleteBuffers(1, &VBO);
+	glDeleteBuffers(1, &EBO);
 	glDeleteProgram(shaderProgram);
 
 	// clean up
