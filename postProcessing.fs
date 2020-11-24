c5d37d906de9bfe154e6a2b5d6737157a7b46fad
Edward Spencer <epfspencer@yahoo.com>
2020-11-24 21:21:16 +0000

    Add blur, sharpen and edges kernels to post processing
    Could make these configurable, but I'm not gonna...

--- postProcessing.fs
+++ postProcessing.fs
@@ -4,9 +4,37 @@ out vec4 FragColor;
 in vec2 TexCoords;
 
 uniform sampler2D screenTexture;
+const float offset = 1.0 / 300.0;
 
 void main()
 {
-    vec3 colour = texture(screenTexture, TexCoords).rgb;
+    vec2 offsets[9] = vec2[](
+        vec2(-offset,  offset),
+        vec2(      0,  offset),
+        vec2( offset,  offset),
+        vec2(-offset,       0),
+        vec2(      0,       0),
+        vec2( offset,       0),
+        vec2(-offset, -offset),
+        vec2(      0, -offset),
+        vec2( offset, -offset)
+    );
+
+    float[9] sharpen = {-1, -1, -1, -1, 9, -1, -1, -1, -1};
+    float[9] blur = {1.0/16, 2.0/16, 1.0/16, 2.0/16, 4.0/16, 2.0/16, 1.0/16, 2.0/16, 1.0/16};
+    float[9] edges = {1, 1, 1, 1, -8, 1, 1, 1, 1};
+
+    vec3 sampleTex[9];
+    for (int i = 0; i < 9; i++)
+    {
+        sampleTex[i] = vec3(texture(screenTexture, TexCoords.st + offsets[i]));
+    }
+
+    vec3 colour = vec3(0);
+    for (int i = 0; i < 9; i++)
+    {
+        colour += sampleTex[i] * edges[i];
+    }
+
     FragColor = vec4(colour, 1);
 }
